e1093a4856d556ac4c35d75f27730f5846ab47c4
hugo-nunes02 <hugo.nunes06012009@gmail.com>
2024-05-08 21:43:20 -0300

    Subida aula 6 e att aulas 3 e 5

---
+++ JavaScript/Aula_3_(Strings)/Aula_3_(Atividade do Dia).js
@@ -0,0 +1,4 @@
+const frutas = [' ', 'Carambola', 'Acerola', 'Maracujá', 'Limão', 'Melancia' ]
+console.log (frutas.length);
+console.log(frutas.trim());
+console.log(frutas.substring(1, 3));
---
+++ JavaScript/Aula_6_(Desvio_Encadeado)/Aula_6_(Desvio_Encadeado).js
@@ -0,0 +1,64 @@
+const num = 'Ola';
+const g = 11;
+
+if(g % 2==0&& !isNaN(g))
+    console.log('Número Par');
+
+else if (g % 2 !=0 && !isNaN(g))
+    console.log('Número Ímpar')
+    
+ else console.log('Não é um número');
+
+ // Operador ternário
+
+ console.clear();
+ let resultado = 3 > 4 ? 'Sim': 'Não';
+ console.log (resultado)
+
+ //Switch e operador ternário
+
+ console.clear();
+ const h = 11;
+ const cor = h > 11? 'vermelha':'azul';
+
+ switch(cor){case'vermelha': console.log("A cor é vermelha!");
+    break;
+    case "azul": console.log("A cor é azul!");
+        break;
+        default: console.log('A cor não é vermelha nem azul')
+ }
+
+ // Array unidimensional
+
+const num1 = new Array (30,-1,5,3,121);
+const num2 = (-2,40,16,111,33,164);
+console.log(num1);
+console.log(num2);
+
+// Acessando elementos diversos do array
+
+console.log(`primeiro elemento de num1: ${num1[0]}`);
+console.log(`primeiro elemento de num2: ${num1[0]}`);
+
+console.log(`primeiro elemento de num1: ${num1[2]}`);
+console.log(`primeiro elemento de num2: ${num2[5]}`);
+
+
+// Estrutura condicional if e else
+
+console.clear();
+
+const e = '10';
+if (e === 10)console.log('E equivale ao número 10');
+
+else{console.log('Resultado da condição: ${e===10}');
+console.log('E não é um número')}
+
+// Array bidimensional
+
+console.clear();
+const matrix=[['Banana','Maça','Pêra'],
+['Laranja',true,1],
+[null,'Uva',-350]];
+
+// Acessa
